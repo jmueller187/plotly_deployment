6547e1ca786955ac8ad94c8a844456bd64065e02
jmueller187 <92120094+jmueller187@users.noreply.github.com>
2022-01-11 10:32:34 -0500

    Resources added through Module 12.5.1.

---
+++ Resources/data-12-4-3-resources/plots.js
@@ -0,0 +1,42 @@
+// Populate 'Test Subject ID No.:' section of index.html.
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+  // Switch between Test Subject ID No's.
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+
+  // Fumction to populate 'Demographic Info' section of index.html.
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text('ID: ' + result.id);
+      PANEL.append("h6").text('ETHNICITY: ' + result.ethnicity);
+      PANEL.append("h6").text('GENDER: ' + result.gender);
+      PANEL.append("h6").text('AGE: ' + result.age);
+      PANEL.append("h6").text('LOCATION: ' + result.location);
+      PANEL.append("h6").text('BBTYPE: ' + result.bbtype);
+      PANEL.append("h6").text('WFREQ: ' + result.wfreq);
+      
+    });
+  }
