331af619075cb8002115522ef74ea221cf5e50aa
jmueller187 <92120094+jmueller187@users.noreply.github.com>
2022-01-13 18:06:53 -0500

    Module 12 Deliverable 3 complete.

--- js/charts.js
+++ js/charts.js
@@ -4,7 +4,7 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
-    // console.log(data);
+    console.log(data);
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -60,13 +60,23 @@ function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var dataSamples = data.samples;
-    console.log(dataSamples);
+    // console.log(dataSamples);
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var resultArray = dataSamples.filter(sampleObj => sampleObj.id == sample);
-    console.log(resultArray);
+    // console.log(resultArray);
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
-    console.log(result);
+    // console.log(result);
+
+    // 1. (Gague Chart) Create a variable that filters the metadata array for the object with the desired sample number.
+    var dataMetadata = data.metadata;
+
+    // (Gague Chart) Create a variable that holds the first sample in the array.
+    var metadataArray = dataMetadata.filter(sampleObj => sampleObj.id == sample);
+  
+    // 2. (Gague Chart) Create a variable that holds the first sample in the metadata array.
+    var metaData = metadataArray[0];
+    console.log(metadataArray);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otuIds = result.otu_ids;
@@ -75,6 +85,10 @@ function buildCharts(sample) {
     // console.log(otuLabels);
     var sampleValues = result.sample_values;
     // console.log(sampleValues);
+
+    // 3. (Gague Chart) Create a variable that holds the washing frequency.
+    var washFreq = parseFloat(metaData.wfreq);
+    console.log(washFreq);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
@@ -121,12 +135,42 @@ function buildCharts(sample) {
     // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: 'Bacteria Cultures Per Sample',
-      showlegend: false,
-
+      xaxis: {title: 'OTU ID'},
+      showlegend: false
     };
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+// Gague Charts
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+      {
+        value: washFreq,
+        title: { text: "<b>Belly Button Washing Frequency</b><br> Scrubs per Week" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: { range: [null, 10] },
+          steps: [
+            { range: [0, 2], color: "red" },
+            { range: [2, 4], color: "orange" },
+            { range: [4, 6], color: "yellow" },
+            { range: [6, 8], color: "yellowgreen" },
+            { range: [8, 10], color: "green" }
+          ],
+          bar: { color: "black" }
+        }
+      }
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 600, height: 450, margin: { t: 0, b: 0 }
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
 
