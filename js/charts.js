80aabd23857aa4ee2c75b620a17574375ed98917
jmueller187 <92120094+jmueller187@users.noreply.github.com>
2022-01-13 11:14:02 -0500

    Module 12 Deliverable 1 complete.

--- js/charts.js
+++ js/charts.js
@@ -80,23 +80,27 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otuIds.slice(0, 10).reverse();
-    console.log(yticks);
+    var yticks = otuIds.slice(0, 10).map(otuIds => `OTU ${otuIds}`).reverse();
+    // console.log(yticks);
+
+    
 
     // 8. Create the trace for the bar chart. 
     trace = {
-      x: sampleValues,
+      x: sampleValues.slice(0, 10).reverse(),
       y: yticks,
-      text: otuLabels,
+      text: otuLabels.slice(0, 10).reverse(),
       type: "bar",
       orientation: "h"
     }
 
     var barData = [trace];
+
     // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacterial Cultures Found",
     };
+    
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
   });
